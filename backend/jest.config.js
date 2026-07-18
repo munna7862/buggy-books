0f444e1713157b9afb8d9136e95eb09a5b0214f1
munna7862 <sadhik7862@gmail.com>
2026-07-18 11:06:53 +0530

    delete changes in gh page

--- backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  clearMocks: true,
-  setupFiles: ['./src/__tests__/setup.ts'],
-};
--- backend/src/config.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-const secret = process.env.JWT_SECRET;
-if (!secret) {
-  throw new Error('FATAL: JWT_SECRET environment variable is not set. Add it to your .env file.');
-}
-
-export const JWT_SECRET: string = secret;
